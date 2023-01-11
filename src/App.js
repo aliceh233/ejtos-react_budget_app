382b1647d9aa704743f33f2ae66fcb3bf7d539d2
aliceh233 <alicehuang233@gmail.com>
2023-01-10 20:18:02 -0500

    init

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm';
+import ExpenseList from './components/ExpenseList';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
+                    <h1> Company's Budget Allocation</h1>
+                    <div className="row">
+                        <div className="col-sm-4"><Budget></Budget></div>
+                        <div className="col-sm-4"><Remaining></Remaining></div>
+                        <div className="col-sm-4"><ExpenseTotal></ExpenseTotal></div>
+                    </div>
+                    <h2>Allocation</h2>
+                        <ExpenseList></ExpenseList>
+                    <h2>Change Allocation</h2>
+                        <AllocationForm></AllocationForm>
             </div>
         </AppProvider>
     );
